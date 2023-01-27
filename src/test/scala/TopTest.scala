0873ff17d7811f7207373acf477c7c32cf5354dd
Laiba Khan <98444317+LaibaKhan1@users.noreply.github.com>
2023-01-27 14:44:05 +0500

    Add files via upload

---
+++ src/test/scala/TopTest.scala
@@ -0,0 +1,13 @@
+package Pipelining
+
+import chisel3._
+import org.scalatest.FreeSpec
+import chiseltest._
+
+class TopTest extends FreeSpec with ChiselScalatestTester{
+   "Top test" in{
+       test(new PipTop ()){c =>
+         c.clock.step(80) 
+       }
+   }
+}
